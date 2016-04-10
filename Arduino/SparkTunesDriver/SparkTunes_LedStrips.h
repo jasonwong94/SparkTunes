f15a36a2ab7b6d90519f0a023b577b2304999383
Jason Wong <jaw.wong@mail.utoronto.ca>
2016-04-10 15:20:17 -0400

    Added a test function to the library to check if the individual R, G and B LEDs work

--- Arduino/SparkTunesDriver/SparkTunes_LedStrips.h
+++ Arduino/SparkTunesDriver/SparkTunes_LedStrips.h
@@ -4,7 +4,9 @@
 #define SPARKTUNES_LEDSTRIPS_H
   const int LEDS_PER_STRIP = 30;
   const int NUM_STRIPS = 2;
+  const int MAX_NUM_LEDS = LEDS_PER_STRIP*2; //change if need be
 
+  void ledStrips_test(Adafruit_NeoPixel *strip);
   void ledStrips_reset(Adafruit_NeoPixel *strip);
   void ledStrips_flashLeds(Adafruit_NeoPixel *strip, int leds[], uint32_t colour[], unsigned long pulse);
   void ledStrips_displayIterations(Adafruit_NeoPixel *strip, int iteration);
